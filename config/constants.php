e5d4178a0784d35dd5cb0ef4492bb6fe13980650
radityacandra <radityacandra@gmail.com>
2016-06-24 23:20:32 +0700

    add constant in config directory

---
+++ config/constants.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * file to save constants name
+ * to set constant from another class/method: Config::set('foo.bar', 'test');
+ * to get constant: Config::get('constants.langs');
+ */
+
+return [
+  'MAX_FILE_SIZE' => 2000,
+  'VALID_FORMAT'  => array("jpg", "png", "gif", "bmp","jpeg")
+];
