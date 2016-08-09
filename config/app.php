12c936e30e2821dda083995cd70da812caf7387f
radityacandra <radityacandra@gmail.com>
2016-08-09 20:05:08 +0700

    add laravel-dompdf dependency

--- config/app.php
+++ config/app.php
@@ -165,6 +165,11 @@ return [
 	       * Laravel QR code service provider
 	       */
 		    SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+	    
+	      /*
+	       * Laravel DomPDF service provider
+	       */
+	      Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -213,6 +218,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 		    'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
+	      'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
